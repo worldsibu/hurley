f8589bb4c1ca2ce77414539a9db6ff452821d6d9
walmon <walmon93@hotmail.com>
2019-03-11 09:11:51 -0600

    feat: improved logs
    Signed-off-by: walmon <walmon93@hotmail.com>

--- src/utils/logs.ts
+++ src/utils/logs.ts
@@ -1,7 +1,16 @@
+import { inspect } from 'util';
 export function l(msg: string | Error) {
     if(typeof msg === 'string'){
         console.log(`[hurley] - ${msg}`);
     }else{
-        console.log(msg);
+        // console.log(inspect(msg, { depth: 5 }));
+        if((msg as any).responses && (msg as any).responses.length > 0){
+            for(let response of (msg as any).responses){
+                console.log(response.Error);
+                console.log(response);
+            }
+        }
+        // console.log((msg as any).responses);
+        // console.log(JSON.stringify(msg, null, 4));
     }
 }
