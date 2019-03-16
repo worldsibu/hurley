58dfa0abe42260e3b4f060510dac4775ec0c3636
walmon <walmon93@hotmail.com>
2019-03-16 12:15:03 -0600

    feat: more details message
    Signed-off-by: walmon <walmon93@hotmail.com>

--- src/utils/logs.ts
+++ src/utils/logs.ts
@@ -1,14 +1,16 @@
 import { inspect } from 'util';
 export function l(msg: string | Error) {
-    if(typeof msg === 'string'){
+    if (typeof msg === 'string') {
         console.log(`[hurley] - ${msg}`);
-    }else{
+    } else {
         // console.log(inspect(msg, { depth: 5 }));
-        if((msg as any).responses && (msg as any).responses.length > 0){
-            for(let response of (msg as any).responses){
+        if ((msg as any).responses && (msg as any).responses.length > 0) {
+            for (let response of (msg as any).responses) {
                 console.log(response.Error);
                 console.log(response);
             }
+        } else {
+            console.log(msg);
         }
         // console.log((msg as any).responses);
         // console.log(JSON.stringify(msg, null, 4));
